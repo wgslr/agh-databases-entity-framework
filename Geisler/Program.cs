2e7a15bbd3be52df5853e892be1f8fb75b3ce504
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-10 23:53:58 +0100

    Add empty windows form

--- Geisler/Program.cs
+++ Geisler/Program.cs
@@ -8,27 +8,28 @@ namespace Geisler
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             ProdContext ctx = new ProdContext();
 
-            while (true)
+
+            System.Console.WriteLine("Specify category name");
+            String name = System.Console.ReadLine();
+            Category cat = new Category();
+            cat.Name = name;
+            ctx.Categories.Add(cat);
+
+            ctx.SaveChanges();
+
+            System.Console.WriteLine("Available categories are:");
+            foreach (Category c in ctx.Categories)
             {
-
-                System.Console.WriteLine("Specify category name");
-                String name = System.Console.ReadLine();
-                Category cat = new Category();
-                cat.Name = name;
-                ctx.Categories.Add(cat);
-
-                ctx.SaveChanges();
-
-                System.Console.WriteLine("Available categories are:");
-                foreach (Category c in ctx.Categories)
-                {
-                    System.Console.WriteLine("- " + c.Name);
-                }
+                System.Console.WriteLine("- " + c.Name);
             }
+
+            CategoryForm f = new CategoryForm();
+            f.ShowDialog();
         }
     }
 }
