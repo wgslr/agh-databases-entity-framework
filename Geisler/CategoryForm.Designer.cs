fbcc55b5e7f5575ac73e2f564b1fae126b8f9752
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-11 02:43:12 +0100

    Display products list

--- Geisler/CategoryForm.Designer.cs
+++ Geisler/CategoryForm.Designer.cs
@@ -35,8 +35,15 @@
             this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.categoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.saveButton = new System.Windows.Forms.Button();
+            this.productsGrid = new System.Windows.Forms.DataGridView();
+            this.productId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unitprice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -51,9 +58,9 @@
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
             this.dataGridView1.Location = new System.Drawing.Point(13, 13);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(600, 250);
+            this.dataGridView1.Size = new System.Drawing.Size(447, 150);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.onSaveClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.onCategorySelected);
             // 
             // categoryIdDataGridViewTextBoxColumn
             // 
@@ -80,7 +87,7 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(13, 270);
+            this.saveButton.Location = new System.Drawing.Point(13, 169);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 1;
@@ -88,17 +95,59 @@
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.onSaveClick);
             // 
+            // productsGrid
+            // 
+            this.productsGrid.AutoGenerateColumns = false;
+            this.productsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productsGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productId,
+            this.dataGridViewTextBoxColumn2,
+            this.Unitprice});
+            this.productsGrid.DataSource = this.productsBindingSource;
+            this.productsGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
+            this.productsGrid.Location = new System.Drawing.Point(13, 198);
+            this.productsGrid.Name = "productsGrid";
+            this.productsGrid.Size = new System.Drawing.Size(447, 150);
+            this.productsGrid.TabIndex = 2;
+            // 
+            // productId
+            // 
+            this.productId.DataPropertyName = "productId";
+            this.productId.HeaderText = "productId";
+            this.productId.Name = "productId";
+            this.productId.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // Unitprice
+            // 
+            this.Unitprice.DataPropertyName = "Unitprice";
+            this.Unitprice.HeaderText = "Unitprice";
+            this.Unitprice.Name = "Unitprice";
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.categoryBindingSource;
+            // 
             // CategoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 366);
+            this.ClientSize = new System.Drawing.Size(670, 455);
+            this.Controls.Add(this.productsGrid);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.dataGridView1);
             this.Name = "CategoryForm";
             this.Text = "CategoryForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -111,5 +160,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView productsGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unitprice;
+        private System.Windows.Forms.BindingSource productsBindingSource;
     }
 }
--- Geisler/CategoryForm.cs
+++ Geisler/CategoryForm.cs
@@ -25,7 +25,10 @@ namespace Geisler
             base.OnLoad(e);
             context = new ProdContext();
             context.Categories.Load();
+            context.Products.Load();
             this.categoryBindingSource.DataSource = context.Categories.Local.ToBindingList();
+            this.productsBindingSource.DataSource = context.Products.Local.ToBindingList();
+            
         }
 
         private void onSaveClick(object sender, EventArgs e)
@@ -33,6 +36,20 @@ namespace Geisler
             context.SaveChanges();
             this.dataGridView1.Refresh();
 
+        }
+
+        private void onSaveClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void onCategorySelected(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            //var category = ;
+            //var categoryId = category.Cells[0];
+
+            Category catObj = (Category) dataGridView1.Rows[row].DataBoundItem;
         }
     }
 }
